206ef8145e1094a7ac022d1e2a4b64556a86c210
Anika Jibben <anikajib@email.unc.edu>
2021-04-21 23:28:59 -0400

    pt 2 of nice

--- exercises/ex05/Simpy.py
+++ exercises/ex05/Simpy.py
@@ -10,19 +10,23 @@ __author__ = "730395244"
 class Simpy:
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+
     def __init__(self, values: list[float]):
-       self.values = values 
+        """Constructor."""
+        self.values = values 
 
     def __repr__(self) -> str:
+        """Printable Representation Method."""
         return f"Simpy({self.values})"
 
     def fill(self, x_val: float, y_index: int) -> None:
+        """First Method to fill List."""
         self.values = []
         for i in range(y_index):
             self.values.append(x_val)
 
     def arange(self, start: float, stop: float, step: float = 1.0) -> None: 
+        """Second Method filling values with range of values."""
         self.values = []
         step != 0.0
         self.values.append(start)
@@ -37,9 +41,11 @@ class Simpy:
                 value += step
 
     def sum(self) -> float:
+        """Sum Method."""
         return sum(self.values)
 
     def __add__(self,rhs: Union[float, Simpy]) -> Simpy:
+        """First operator overload to add."""
         final: list[float] = []
         if isinstance(rhs, float):
             for item in self.values:
@@ -52,6 +58,7 @@ class Simpy:
         return Simpy(final)
 
     def __pow__(self, power: Union[float, Simpy]) -> Simpy:
+        """Second operator overload to raise to a power."""
         final: list[float] = []
         if isinstance(power, float):
             for item in self.values:
@@ -64,6 +71,7 @@ class Simpy:
         return Simpy(final)
 
     def __mod__(self, remain: Union[float, Simpy]) -> Simpy:
+        """Third operator overload to use remainder."""
         final: list[float] = []
         if isinstance(remain, float):
             for item in self.values:
@@ -76,6 +84,7 @@ class Simpy:
         return Simpy(final)
 
     def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Fourth o.o. for equal."""
         result: list[bool] = []
         if isinstance(rhs, float):
             for item in self.values:
@@ -88,6 +97,7 @@ class Simpy:
         return result
 
     def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Fifth o.o. for greater than."""
         result: list[bool] = []
         if isinstance(rhs, float):
             for item in self.values:
@@ -100,6 +110,7 @@ class Simpy:
         return result
 
     def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
+        """Final o.o."""
         return self.values[rhs]
 
         
